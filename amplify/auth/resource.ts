0a55f5c4ca6db0c70617f8e3b39995528dbca4dc
XIE ZHIFEN <xiezhifen@graduate.utm.my>
2024-11-17 18:51:23 +0800

    added per-user data isolation

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,9 +4,6 @@ export const auth = defineAuth({
   loginWith: {
     email: true, // 启用邮箱登录
   },
-  attributes: {
-    required: ['email'], // 确保邮箱是必填字段
-  },
   verification: {
     email: true, // 注册后需要邮箱验证
   },
